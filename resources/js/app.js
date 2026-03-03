8b7204433fb1e19b63bc8ebdae5971d058a26804
mkdevtz04 <costantineclement6@gmail.com>
2026-03-03 10:25:22 +0300

    costal charm bnb

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+import flatpickr from 'flatpickr';
+import Alpine from 'alpinejs';
+import "flatpickr/dist/flatpickr.min.css";
+
+window.flatpickr = flatpickr;
+window.Alpine = Alpine;
+
+Alpine.start();
