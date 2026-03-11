7fc6c8ab65bb01d3f824522727cd10744b23ef95
mkdevtz04 <costantineclement6@gmail.com>
2026-03-11 12:33:14 +0300

    sh

--- docker/run.sh
+++ docker/run.sh
@@ -5,6 +5,14 @@ chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Run migrations if database is available
 # php artisan migrate --force
+#!/bin/sh
+
+# Clear caches to ensure new Env variables are picked up
+php artisan config:clear
+php artisan cache:clear
+
+# Run migrations (the --force flag is required for production)
+php artisan migrate --force
 
 # Start Apache
 apache2-foreground
